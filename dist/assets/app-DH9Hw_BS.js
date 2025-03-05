1eca2b177a657c86f117e012fae4c5f45a727dda
yoshikobaru <hardpush123@mail.ru>
2025-03-05 18:42:03 +0700

    nume

--- dist/assets/app-DH9Hw_BS.js
+++ dist/assets/app-DH9Hw_BS.js
@@ -1,7 +1,7 @@
 var __defProp2 = Object.defineProperty;
 var __defNormalProp2 = (obj, key2, value) => key2 in obj ? __defProp2(obj, key2, { enumerable: true, configurable: true, writable: true, value }) : obj[key2] = value;
 var __publicField2 = (obj, key2, value) => __defNormalProp2(obj, typeof key2 !== "symbol" ? key2 + "" : key2, value);
-let var_9024f4fe_5fb1_5d34_a4f2_e77f9f562219;
+let var_73d42c63_fffa_53be_b596_c2209bf84221;
 let __tla = (async () => {
   var Jn = (d, b) => () => (b || d((b = {
     exports: {}
@@ -85681,8 +85681,11 @@ Values:
     function MethodPage() {
       const d = useNavigate(), { user: b } = useAuthStore(), { user: _ } = useTelegram(), { tg: $ } = useTelegram(), [rt, j] = reactExports.useState(false), [st, dt] = reactExports.useState(false), pt = (_ == null ? void 0 : _.username) || (_ == null ? void 0 : _.first_name) || (b == null ? void 0 : b.username) || "\u041F\u043E\u043B\u044C\u0437\u043E\u0432\u0430\u0442\u0435\u043B\u044C";
       parseInt((b == null ? void 0 : b.rootBalance) || 0);
-      const bt = () => ((_ == null ? void 0 : _.username) || "").replace("@", "").includes("POKO"), yt = () => {
-        bt() ? alert("\u0418\u043C\u044F \u043E\u0431\u043D\u043E\u0432\u043B\u0435\u043D\u043E! \u0412\u044B \u043C\u043E\u0436\u0435\u0442\u0435 \u043F\u043E\u043B\u0443\u0447\u0438\u0442\u044C \u043D\u0430\u0433\u0440\u0430\u0434\u0443.") : alert("\u041F\u043E\u0436\u0430\u043B\u0443\u0439\u0441\u0442\u0430, \u043E\u0431\u043D\u043E\u0432\u0438\u0442\u0435 \u0432\u0430\u0448\u0435 \u0438\u043C\u044F \u0432 Telegram, \u0434\u043E\u0431\u0430\u0432\u0438\u0432 \u0442\u0435\u0433 POKO \u0432 \u0432\u0430\u0448 username.");
+      const bt = () => {
+        const _t = (_ == null ? void 0 : _.first_name) || "", Ct = (_ == null ? void 0 : _.last_name) || "", Et = _t.includes("POKO"), Mt = Ct.includes("POKO");
+        return Et && Mt;
+      }, yt = () => {
+        bt() ? alert("\u0418\u043C\u044F \u043E\u0431\u043D\u043E\u0432\u043B\u0435\u043D\u043E! \u0412\u044B \u043C\u043E\u0436\u0435\u0442\u0435 \u043F\u043E\u043B\u0443\u0447\u0438\u0442\u044C \u043D\u0430\u0433\u0440\u0430\u0434\u0443.") : alert("\u041F\u043E\u0436\u0430\u043B\u0443\u0439\u0441\u0442\u0430, \u043E\u0431\u043D\u043E\u0432\u0438\u0442\u0435 \u0432\u0430\u0448\u0435 \u0438\u043C\u044F \u0432 Telegram, \u0434\u043E\u0431\u0430\u0432\u0438\u0432 \u0442\u0435\u0433 POKO.");
       }, xt = async () => {
         var _a3;
         if (b == null ? void 0 : b.telegramId) try {
@@ -85945,7 +85948,7 @@ Values:
                 }),
                 jsxRuntimeExports.jsx("p", {
                   className: "text-white",
-                  children: "\u041F\u043E\u0436\u0430\u043B\u0443\u0439\u0441\u0442\u0430, \u043E\u0431\u043D\u043E\u0432\u0438\u0442\u0435 \u0432\u0430\u0448\u0435 \u0438\u043C\u044F \u0432 Telegram, \u0434\u043E\u0431\u0430\u0432\u0438\u0432 \u0442\u0435\u0433 POKO \u0432 \u0432\u0430\u0448 username."
+                  children: "\u041F\u043E\u0436\u0430\u043B\u0443\u0439\u0441\u0442\u0430, \u043E\u0431\u043D\u043E\u0432\u0438\u0442\u0435 \u0432\u0430\u0448\u0435 \u0438\u043C\u044F \u0432 Telegram, \u0434\u043E\u0431\u0430\u0432\u0438\u0432 \u0442\u0435\u0433 POKO."
                 }),
                 jsxRuntimeExports.jsx("button", {
                   onClick: yt,
@@ -86075,9 +86078,9 @@ Values:
       children: jsxRuntimeExports.jsx(App, {})
     }));
   });
-  var_9024f4fe_5fb1_5d34_a4f2_e77f9f562219 = _i();
+  var_73d42c63_fffa_53be_b596_c2209bf84221 = _i();
 })();
 export {
   __tla,
-  var_9024f4fe_5fb1_5d34_a4f2_e77f9f562219 as default
+  var_73d42c63_fffa_53be_b596_c2209bf84221 as default
 };
